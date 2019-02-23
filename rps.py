43a0284e4eb34e5bb00475a4845e559047b11af7
Loyola User <Confer40@luc.edu>
2019-02-23 11:42:27 -0600

    Rock Paper Sciccors 2/23/19

--- rps.py
+++ rps.py
@@ -1,27 +1,46 @@
 import random
 
-print("Welcome to Rock, Paper, Scissors!")
- 
-player = raw_input("Rock, Paper, or Scissors?")
+print ("Welcome to Rock, Paper, Scissors!")
 
-computer = random.randint(1,3)
+while user_wins < 3 and computer_wins < 3:
+	user_wins = 0
+	computer_wins = 0
+	user_choice = raw_input("Rock, Paper, or scissors?")
 
-#int 1 = "Rock"
-#int 2 = "Paper"
-#int 3 = "Scissors"
+	for x in range(3):
+		computer_choice = random.randint(1,3),
 
-computer_choice = 0
-if computer == 1:
-	computer_choice = ("Rock")
-	print("Rock")
-elif computer == 2:
-	computer_choice = ("Paper")
-	print("Paper")
-elif computer == 3:
-	computer_choice = ("Scissors")
-	print("Scissors")
-else: 
-	print("Error")
- 
+	computer_word = ""
+	if computer_choice == 1:
+		computer_word = "rock"
+	elif conputer_choice == 2:
+		computer_word = "paper"
+	elif conputer_choice == 3:
+		computer_word = "scissors"
 
-	
+	if user_choice == "rock" and computer_word == "paper":
+		print ("COMPUTER WINS")
+		COMPUTER_WINS = COMPUTER_WINS + 1
+	elif user_choice == "rock" and computer_word == "scissors":
+		print ("YOU WIN")
+		YOU_WIN = YOU_WIN + 1
+	elif user_choice == "rock" and computer_word == "rock":
+		print ("TIE")
+	elif user_choice == "paper" and computer_word == "rock":
+		print ("YOU WIN")
+		YOU_WIN = YOU_WIN + 1
+	elif user_choice == "paper" and computer_word == "scissors":
+		print ("COMPUTER WINS")
+		COMPUTER_WINS = COMPUTER_WINS + 1
+	elif user_choice == "paper" and computer_word == "paper":
+		print ("TIE")
+	elif user_choice == "scissors" and computer_word == "paper":
+		print ("YOU WIN")
+		YOU_WIN = YOU_WIN + 1
+	elif user_choice == "scissors" and computer_word == "scissors":
+		print ("TIE")
+	elif user_choice == "scissors" and computer_word == "rock":
+		print ("COMPUTER WINS")
+		COMPUTER_WINS = COMPUTER_WINS + 1
+	else:
+		print ("Error")
